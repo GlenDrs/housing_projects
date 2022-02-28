c71ced3483afbef5aceb49202bc5bb0e4e25714b
GlenDu <glen_llaci@yahoo.com>
2022-02-28 22:01:28 +0100

    test 2

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 require_relative '../lib/house'
 
 RSpec.describe House do
-    it 'initialize length' do
-      expect(House.new(length: 1, width: 1, floors: 1, price_m2: 1.0)).to be_a House
+  subject(:house) { described_class.new(length: 1, width: 1, floors: 1, price_m2: 1.0) }
+  it 'initialize length' do
+    expect(house).to be_a House
+  end
+  context '#perimetre'
+  it do
+    expect(house.perimeter).not_to be_empty
+    expect(house.perimeter).to eq 4
   end
 end
