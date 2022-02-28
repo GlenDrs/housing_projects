31643ec1e75b3eabd1701e7fef4cd4c92c7c2fc1
GlenDu <glen_llaci@yahoo.com>
2022-02-28 21:25:04 +0100

    first test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 require_relative '../lib/house'
 
 RSpec.describe House do
-
+    it 'initialize length' do
+      expect(House.new(length: 1, width: 1, floors: 1, price_m2: 1.0)).to be_a House
+  end
 end
