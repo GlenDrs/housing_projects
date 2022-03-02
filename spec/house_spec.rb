8206ff429d0a437e11078ed0d1a75732bfb827d9
GlenDu <glen_llaci@yahoo.com>
2022-03-02 17:40:23 +0100

    correction house_spec

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -3,38 +3,63 @@ require_relative '../lib/house'
 
 RSpec.describe House do
   subject(:house) { described_class.new(length: 1, width: 1, floors: 1, price_m2: 1.0) }
-  it 'initialize length' do
+
+  it 'initialize House' do
     expect(house).to be_a House
-    expect(house.length).to be_a Integer
-    expect(house.length).to eq 1
-    expect(house.width).to be_a Integer
-    expect(house.width).to eq 1
-    expect(house.floors).to be_a Integer
-    expect(house.floors).to eq 1
-    expect(house.price_m2).to be_a Float
-    expect(house.price_m2).to eq 1.0
-    expect(house.created_at).to be_a Date
-  end
-  context '#perimetre'
-  it do
-    expect(house.perimeter).to be_a Integer
-    expect(house.perimeter).to eq 4
   end
 
-  context '#surface_area'
-  it do
-    expect(house.surface_area).to be_a Integer
-    expect(house.surface_area).to eq 1
+  describe '#length' do
+    it 'testing length attribute' do
+      expect(house.length).to eq 1
+    end
   end
 
-  context '#price'
-  it do
-    expect(house.price).to be_a Float
-    expect(house.price).to eq 1.0
+  describe '#width' do
+    it 'testing width attribute' do
+      expect(house.width).to eq 1
+    end
   end
 
-  it '#display_price' do
-    expect(house.display_price).to be_a String
-    expect(house.display_price).to eq("1.0 $")
+  describe '#floors' do
+    it 'testing floors attribute' do
+      expect(house.floors).to eq 1
+    end
+  end
+
+  describe '#created_at' do
+    it 'testing created_at attribute' do
+      expect(house.created_at).to eq Date.today
+  end
+
+  describe '#price_m2' do
+    it 'Price for square meter test' do
+      expect(house.price_m2).to eq 1.0
+    end
+  end
+
+  describe '#perimetre' do
+    it 'The perimeter method test' do
+      expect(house.perimeter).to eq 4
+    end
+  end
+
+  describe '#surface_area' do
+    it 'The surface method test' do
+      expect(house.surface_area).to eq 1
+    end
+  end
+
+  describe '#price' do
+    it 'The price of the house test' do
+      expect(house.price).to eq 1.0
+    end
+  end
+
+  describe '#display_price' do
+    it 'Show price in $ method test' do
+      expect(house.display_price).to eq("1.0 $")
+    end
+  end
+
   end
 end
